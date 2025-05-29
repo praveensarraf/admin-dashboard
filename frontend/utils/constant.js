e84b284137bedebec0a9a797fe5de967c781074e
praveensarraf <praveen96650@gmail.com>
2025-05-29 22:31:03 +0530

    Frontend connected to server

---
+++ frontend/utils/constant.js
@@ -0,0 +1,5 @@
+export const USER_API_ENDPOINT = 'https://admin-dashboard-znez.onrender.com/api/v1/user'
+
+export const AGENTS_API_ENDPOINT = 'https://admin-dashboard-znez.onrender.com/api/v1/agents'
+
+export const TASKS_API_ENDPOINT = 'https://admin-dashboard-znez.onrender.com/api/v1/tasks'
